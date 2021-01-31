c768787b9c8e2a94f58db7abf6a45d2d73a1c313
Gabriel <gabrielkollun@gmail.com>
2021-01-31 08:43:48 -0300

    Corrections

--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -11,10 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-<<<<<<< HEAD
 using ProAgil.WebAPI.Data;
-=======
->>>>>>> 88e21c4ea2c92bc13574e3fbcb8ca571917b17fd
 
 namespace ProAgil.WebAPI
 {
@@ -30,11 +27,8 @@ namespace ProAgil.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-<<<<<<< HEAD
             services.AddDbContext<DataContext>(
                 x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-=======
->>>>>>> 88e21c4ea2c92bc13574e3fbcb8ca571917b17fd
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -44,7 +38,6 @@ namespace ProAgil.WebAPI
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-<<<<<<< HEAD
             }
             else
             {
@@ -53,16 +46,6 @@ namespace ProAgil.WebAPI
             }
 
             //app.UseHttpsRedirection();
-=======
-            }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
->>>>>>> 88e21c4ea2c92bc13574e3fbcb8ca571917b17fd
             app.UseMvc();
         }
     }
